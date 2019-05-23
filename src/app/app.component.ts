ef5dc8b96776466487f1432793d78730822565e0
Inácio Fabrício Nunes de Sousa <fabricionunes.si@hotmail.com>
2019-05-23 09:30:29 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const fbq: any; // É necessario declara essa constante.
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'exemplo-fbq-pixel';
+
+  ngOnInit() {
+
+    // Chama função para fazer o tracking
+    fbq('track', 'Lead', {
+      url: 'http://seu-dominio.com.br/view-atual'
+    });
+
+  }
 }
